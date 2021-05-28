6bc527feecb5a7aaa555b5a0d68fb8ba75992aa4
luiz-flamorim <luiz.flamorim@gmail.com>
2021-05-28 11:19:13 +0100

    working on the grid

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ async function getData(url) {
   drawGrid(data)
 }
 
-function drawGrid(number) {
+function drawGrid(data) {
 
-  let width = 750
-  let height = 750
+  console.log(data)
+
+  let margin = {
+    top: 50,
+    bottom: 50,
+    left: 50,
+    right: 50
+  }
+
+  let width = 750 - margin.left - margin.right
+  let height = 750 - margin.top - margin.bottom
+
   let svgBackgroundColor = '#cecece';
 
   let svg = d3.select('#grid-chart')
@@ -19,32 +29,31 @@ function drawGrid(number) {
     .attr("height", height)
     .style('background-color', svgBackgroundColor);
 
-  let numCols = Math.floor(number[0] / 20);
-  let numRows = 30;
-
-  console.log(numCols)
+  let numCols = 20;
+  let numRows = Math.ceil(data[0] / 20); // returns 34
 
   let y = d3.scaleBand()
-    .range([0, number[0]])
+    .range([0, data[0]])
     .domain(d3.range(numRows));
 
   let x = d3.scaleBand()
     .range([0, 250])
     .domain(d3.range(numCols));
 
-  let data = d3.range(numCols * numRows);
+  let dataGrid = d3.range(numCols * numRows);
 
   let container = svg.append("g")
     .attr("transform", "translate(135,130)");
 
   container.selectAll("circle")
-    .data(data)
+    .data(data[1])
     .enter().append("circle")
     .attr("id", function (d) {
-      return "id" + d;
+      return "id" + d.id;
     })
     .attr('cx', function (d) {
-      return x(d % numCols);
+      // console.log(d.id % numCols)
+      return x(d.id % numCols);
     })
     .attr('cy', function (d) {
       return y(Math.floor(d / numCols));
@@ -52,6 +61,23 @@ function drawGrid(number) {
     .attr('r', 12)
     .attr('fill', 'white')
     .style('stroke', 'black');
+
+  //   container.selectAll("circle")
+  //     .data(dataGrid)
+  //     .enter().append("circle")
+  //     .attr("id", function (d) {
+  //       return "id" + d;
+  //     })
+  //     .attr('cx', function (d) {
+  //       return x(d % numCols);
+  //     })
+  //     .attr('cy', function (d) {
+  //       return y(Math.floor(d / numCols));
+  //     })
+  //     .attr('r', 12)
+  //     .attr('fill', 'white')
+  //     .style('stroke', 'black');
+
 }
 
 async function fetchData(url) {
